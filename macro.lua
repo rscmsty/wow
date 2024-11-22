4cb5c5303be4852f7b5e6ed774dd78dcd8afac0e
Roger Caine <rscmsty@outlook.com>
2024-11-23 01:01:48 +0800

    Update macro.lua

--- macro.lua
+++ macro.lua
@@ -5,6 +5,7 @@ for i = 1, 12 do
     _G["ActionButton"..i.."HotKey"]:Hide()
 end
 
+-- seems like it does not work, reload as walkaround
 -- 恢复动作条快捷键说明
 for i = 1, 12 do
     _G["ActionButton"..i.."HotKey"]:Show()
